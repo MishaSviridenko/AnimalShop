47b17471da21c390261c4d3723e42ddd41994a4d
Misha Sviridenko <sviridenko_m_a@mail.ru>
2020-10-09 13:53:28 +0300

    annotations_Basket/Category

--- src/main/java/com/andersen/training/model/Basket.java
+++ src/main/java/com/andersen/training/model/Basket.java
@@ -1,12 +1,25 @@
 package com.andersen.training.model;
 
+import lombok.*;
+
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "basket")
+@NoArgsConstructor
+@Data
 public class Basket {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "basket_id")
     private long basketId;
-    private User user; //buyer
+
+    @OneToMany(fetch = FetchType.EAGER) //OneDirection
     private List<Product> products;
-    private int productNumberInBasket;//?????
+
+    @Column(name = "product_count_in_basket")
+    private int productCountInBasket;
 
 }
--- src/main/java/com/andersen/training/model/Category.java
+++ src/main/java/com/andersen/training/model/Category.java
@@ -1,11 +1,25 @@
 package com.andersen.training.model;
 
+import lombok.*;
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "category")
+@NoArgsConstructor
+@Data
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "category_id")
     private long categoryId;
+
+    @Column(name = "category_name", nullable = false)
     private String categoryName;
+
+    @ManyToMany
+    @JoinColumn(name = "categories")
     private List<Product> products;
 
 }
